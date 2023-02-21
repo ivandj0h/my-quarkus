92c58926b88d1defb883eb26edf91950c0846b7b
ivandjoh <ivandi.djoh@gmail.com>
2023-02-22 03:18:20 +0800

    added dependencies

---
+++ src/main/java/com/ivandjoh/GreetingService.java
@@ -0,0 +1,11 @@
+package com.ivandjoh;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    public String greeting(String name) {
+        return "hello " + name;
+    }
+}
---
+++ src/test/java/com/ivandjoh/GreetingResourceTest.java
@@ -0,0 +1,21 @@
+package com.ivandjoh;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/api/v1/")
+          .then()
+             .statusCode(200)
+             .body(is("Welcome to QUARKUS!"));
+    }
+
+}
