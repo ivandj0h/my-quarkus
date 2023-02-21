81a11e1dbe2fbd16dc061cdd67223a89c52c10b2
ivandjoh <ivandi.djoh@gmail.com>
2023-02-22 02:30:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# My Quarkus
+
+## Description
+Rest API with Quarkus & Postgres
+
+```
+This Application will continue to be updated, so please wait for the next update.
+```
---
+++ src/main/java/org/acme/App.java
@@ -0,0 +1,28 @@
+package org.acme;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api/v1")
+public class App {
+
+    @Inject
+    GreetingService greetingService;
+
+    @GET
+    @Path("")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Welcome to QUARKUS!";
+    }
+
+    @GET
+    @Path("/greeting/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String greeting(String name) {
+        return greetingService.greeting(name);
+    }
+}
